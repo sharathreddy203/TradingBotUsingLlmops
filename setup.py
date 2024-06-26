c13f70bb2911c1a541ff0720fd35a96ab46ffe90
sharathreddy203 <sharathreddy2030@gmail.com>
2024-06-26 11:38:21 -0700

    updated notebook

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages, setup
 setup(
     name="financebot",
     version="0.0.1",
-    author="sunny",
-    author_email="sunny.savita@ineuron.ai",
+    author="Madhirala Sharath Reddy",
+    author_email="sharathreddy2030@gmail.com",
     packages =find_packages(),
     install_requires=["langchain","langchain-openai","langchain-astradb","datasets","pypdf","python-dotenv","flask"]
 )
